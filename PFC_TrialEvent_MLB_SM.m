b2beda8d5393f0af7d9f5e596dd110584e8684f3
gaelias <gelias@uci.edu>
2021-07-20 14:31:35 -0700

    Create PFC_TrialEvent_MLB_SM.m
    Initial Commit
    
    erp.... this is actually the trial MLB which needs to have it's methods rewritten in light of the creation of the MLB_SM class.
    
    This is for analyzing and summarizing single units based on the old uniSum analyses included in the StatMatrixNeuralAnalysis package.

---
+++ PFC_TrialEvent_MLB_SM.m
@@ -0,0 +1,311 @@
+classdef PFC_TrialEvent_MLB_SM < MLB_SM
+    %% Properties
+    properties % Parameters
+        pokeInWindow = [-500 500]
+        pokeOutWindow = [-500 500]
+    end
+    properties % Organized Spiking Data
+        pokeInTrialMtx
+        pokeOutTrialMtx
+        trialSpikeMtx
+        fisSeqSpikeMtx
+    end
+    properties % Data Organization Vectors
+        pokeInTrialTime
+        pokeOutTrialTime
+        fisSeqTrlNums
+        osTrlNums
+        skpTrlNums
+        repTrlNums
+        taoTrlNums
+        taRepTrlNums
+        taSkpTrlNums
+    end
+    properties % Posterior Decoding Logicals
+        fisSeqSpikeTimeLog
+        fisSeqSpikeOdorLog
+        fisSeqSpikeTrialPeriodLog
+    end
+    properties % Trial Logicals
+        trialPeriodTimeLog
+        trialTimeLog
+    end
+    properties % Posterior Distributions
+        fisSeqPosts
+        osPosts
+        skpPosts
+        repPosts
+        taoPosts
+        taRepPosts
+        taSkpPosts
+    end
+    properties % Decodings
+        fisL1OdecodeOdrTime
+        fisL1OdecodeTimeTime
+        fisL1OdecodeOdr
+        fisL1OdecodeOdr_TrlPrd
+        fisL1OdecodeTime
+        fisL1OdecodeTime_TrlPrd
+        fisL1OdecodePosts
+        fisL1OdecodePosts_TrlPrd
+        
+        osDecode
+        osDecode_TrlPrd
+        repDecode
+        repDecode_TrlPrd
+        skpDecode
+        skpDecode_TrlPrd
+        
+        taoDecode
+        taoDecode_TrlPrd
+        taRepDecode
+        taRepDecode_TrlPrd
+        taSkpDecode
+        taSkpDecode_TrlPrd
+        taoDecodeTime
+        taoDecodeTime_TrlPrd
+    end
+    
+    %% Methods
+    methods
+        %% Object Creation
+        %#ok<*EXIST>
+        function obj = PFC_TrialEvent_MLB_SM(fileDir, binSize, dsRate, piWindow, poWindow)
+            if nargin == 0
+                fileDir = uigetdir;
+            end
+            obj@MLB_SM(fileDir);
+            if nargin ~= 0
+                if exist('binSize')==1
+                    obj.binSize = binSize;
+                end
+                if exist('dsRate')==1
+                    obj.dsRate = dsRate;
+                end
+                if exist('piWindow')==1
+                    obj.pokeInWindow = piWindow;
+                end
+                if exist('poWindow')==1
+                    obj.pokeOutWindow = poWindow;
+                end
+                obj.CompileMLBmtx('PokeIn');
+                obj.CompileMLBmtx('PokeOut');
+                obj.CompileTrialObsvs;
+                obj.CompileFISlikes;
+                obj.DecodeFIS_L1O;
+                obj.DecodeOS;
+                obj.DecodeTAO;
+            else
+            end
+            
+        end
+    end
+    methods
+        %% Recompile the data
+        function RunAnalysis(obj)
+            fprintf('Analyzing\n');
+            obj.CompileMLBmtx('PokeIn');
+            obj.CompileMLBmtx('PokeOut');
+            obj.CompileTrialObsvs;
+            obj.CompileFISlikes;
+            obj.DecodeFIS_L1O;
+            obj.DecodeOS;
+            obj.DecodeTAO;
+        end
+        %% Decode Trial After OutSeq from FIS
+        function DecodeTAO(obj)
+            if isempty(obj.trialSpikeMtx)
+                obj.CompileTrialObsvs;
+            end
+            obj.taoPosts = nan(size(obj.trialSpikeMtx{1},1), size(obj.fisSeqSpikeMtx,1), length(obj.taoTrlNums));
+            obj.taRepPosts = nan(size(obj.trialSpikeMtx{1},1), size(obj.fisSeqSpikeMtx,1), length(obj.taRepTrlNums));
+            obj.taSkpPosts = nan(size(obj.trialSpikeMtx{1},1), size(obj.fisSeqSpikeMtx,1), length(obj.taSkpTrlNums));
+            obj.taoDecode = nan(size(obj.trialSpikeMtx{1},1), length(obj.taoTrlNums));
+            obj.taoDecode_TrlPrd = nan((size(obj.trialSpikeMtx{1},1)-2)/4, length(obj.taoTrlNums), 4);
+            obj.taRepDecode = nan(size(obj.trialSpikeMtx{1},1), length(obj.taRepTrlNums));
+            obj.taRepDecode_TrlPrd = nan((size(obj.trialSpikeMtx{1},1)-2)/4, length(obj.taRepTrlNums), 4);
+            obj.taSkpDecode = nan(size(obj.trialSpikeMtx{1},1), length(obj.taSkpTrlNums));
+            obj.taSkpDecode_TrlPrd = nan((size(obj.trialSpikeMtx{1},1)-2)/4, length(obj.taSkpTrlNums), 4);
+            obj.taoDecodeTime = nan(size(obj.trialSpikeMtx{1},1), length(obj.taoTrlNums));
+            obj.taoDecodeTime_TrlPrd = nan((size(obj.trialSpikeMtx{1},1)-2)/4, length(obj.taoTrlNums), 4);
+            for taO = 1:length(obj.taoTrlNums)
+                curObsv = obj.trialSpikeMtx{obj.taoTrlNums(taO)};
+                obj.taoPosts(:,:,taO) = obj.CalcStaticBayesPost(mean(obj.fisSeqSpikeMtx,3), curObsv);
+                [tempDecodeOdr, tempDecodeOdrPost] = obj.DecodeBayesPost(obj.taoPosts(:,:,taO), obj.fisSeqSpikeOdorLog);
+                tempDecodeOdr(tempDecodeOdr==obj.trialInfo(obj.taoTrlNums(taO)).Position) = 0;
+                tempDecodeOdr(tempDecodeOdr==obj.trialInfo(obj.taoTrlNums(taO)-1).Position) = -1;
+                tempDecodeOdr(tempDecodeOdr==obj.trialInfo(obj.taoTrlNums(taO)-1).Odor) = -2;
+                tempDecodeOdr(tempDecodeOdr>0) = 1;
+                
+                obj.taoDecode(:,taO) = tempDecodeOdr;
+                
+                obj.taoDecodeTime(:,taO) = obj.DecodeBayesPost(obj.taoPosts(:,:,taO), obj.fisSeqSpikeTimeLog) - obj.trialTimeLog;
+                for prd = 1:4
+                    obj.taoDecode_TrlPrd(:,taO,prd) = tempDecodeOdr(obj.trialPeriodTimeLog==prd);
+                    obj.taoDecodeTime_TrlPrd(:,taO,prd) = obj.taoDecodeTime(obj.trialPeriodTimeLog==prd,taO);
+                end
+                
+                if sum(obj.taoTrlNums(taO)==obj.taSkpTrlNums)==1
+                    obj.taSkpPosts(:,:,obj.taoTrlNums(taO)==obj.taSkpTrlNums) = obj.taoPosts(:,:,taO);
+                    obj.taSkpDecode(:,obj.taoTrlNums(taO)==obj.taSkpTrlNums) = obj.taoDecode(:,taO);
+                    obj.taSkpDecode_TrlPrd(:,obj.taoTrlNums(taO)==obj.taSkpTrlNums,:) = obj.taoDecode_TrlPrd(:,taO,:);
+                elseif sum(obj.taoTrlNums(taO)==obj.taRepTrlNums)==1
+                    obj.taRepPosts(:,:,obj.taoTrlNums(taO)==obj.taRepTrlNums) = obj.taoPosts(:,:,taO);
+                    obj.taRepDecode(:,obj.taoTrlNums(taO)==obj.taRepTrlNums) = obj.taoDecode(:,taO);
+                    obj.taRepDecode_TrlPrd(:,obj.taoTrlNums(taO)==obj.taRepTrlNums,:) = obj.taoDecode_TrlPrd(:,taO,:);
+                end
+            end
+        end
+        %% Decode OutSeq from FIS
+        function DecodeOS(obj)
+            if isempty(obj.trialSpikeMtx)
+                obj.CompileTrialObsvs;
+            end
+            obj.osPosts = nan(size(obj.trialSpikeMtx{1},1), size(obj.fisSeqSpikeMtx,1), length(obj.osTrlNums));
+            obj.osDecode = nan(size(obj.trialSpikeMtx{1},1), length(obj.osTrlNums));
+            obj.osDecode_TrlPrd = nan((size(obj.trialSpikeMtx{1},1)-2)/4, length(obj.osTrlNums), 4);
+            obj.skpPosts = nan(size(obj.trialSpikeMtx{1},1), size(obj.fisSeqSpikeMtx,1), length(obj.skpTrlNums));
+            obj.skpDecode = nan(size(obj.trialSpikeMtx{1},1), length(obj.skpTrlNums));
+            obj.skpDecode_TrlPrd = nan((size(obj.trialSpikeMtx{1},1)-2)/4, length(obj.skpTrlNums), 4);
+            obj.repPosts = nan(size(obj.trialSpikeMtx{1},1), size(obj.fisSeqSpikeMtx,1), length(obj.repTrlNums));
+            obj.repDecode = nan(size(obj.trialSpikeMtx{1},1), length(obj.repTrlNums));
+            obj.repDecode_TrlPrd = nan((size(obj.trialSpikeMtx{1},1)-2)/4, length(obj.repTrlNums), 4);
+            for osT = 1:length(obj.osTrlNums)
+                curObsv = obj.trialSpikeMtx{obj.osTrlNums(osT)};
+                obj.osPosts(:,:,osT) = obj.CalcStaticBayesPost(mean(obj.fisSeqSpikeMtx,3), curObsv);
+                [tempDecode, tempDecodePost] = obj.DecodeBayesPost(obj.osPosts(:,:,osT), obj.fisSeqSpikeOdorLog);
+                tempDecode(tempDecode==obj.trialInfo(obj.osTrlNums(osT)).Position) = 0;
+                tempDecode(tempDecode==obj.trialInfo(obj.osTrlNums(osT)).Odor) = -1;
+                tempDecode(tempDecode>0) = 1;
+                
+                obj.osDecode(:,osT) = tempDecode;
+                for prd = 1:4
+                    obj.osDecode_TrlPrd(:,osT,prd) = tempDecode(obj.trialPeriodTimeLog==prd);
+                end
+                
+                if sum(obj.osTrlNums(osT)==obj.skpTrlNums)==1
+                    obj.skpPosts(:,:,obj.osTrlNums(osT)==obj.skpTrlNums) = obj.osPosts(:,:,osT);
+                    obj.skpDecode(:,obj.osTrlNums(osT)==obj.skpTrlNums) = obj.osDecode(:,osT);
+                    obj.skpDecode_TrlPrd(:,obj.osTrlNums(osT)==obj.skpTrlNums,:) = obj.osDecode_TrlPrd(:,osT,:);
+                elseif sum(obj.osTrlNums(osT)==obj.repTrlNums)==1
+                    obj.repPosts(:,:,obj.osTrlNums(osT)==obj.repTrlNums) = obj.osPosts(:,:,osT);
+                    obj.repDecode(:,obj.osTrlNums(osT)==obj.repTrlNums) = obj.osDecode(:,osT);
+                    obj.repDecode_TrlPrd(:,obj.osTrlNums(osT)==obj.repTrlNums,:) = obj.osDecode_TrlPrd(:,osT,:);
+                end
+            end
+        end
+        %% Decode FIS via Leave-1-Out
+        function DecodeFIS_L1O(obj)
+            if isempty(obj.fisSeqSpikeMtx)
+                obj.CompileFISlikes;
+            end
+            obj.fisSeqPosts = nan(size(obj.fisSeqSpikeMtx,1), size(obj.fisSeqSpikeMtx,1), size(obj.fisSeqSpikeMtx,3));
+            for s = 1:size(obj.fisSeqSpikeMtx,3)
+                tempISS = obj.fisSeqSpikeMtx;
+                tempISS(:,:,s) = [];
+                obj.fisSeqPosts(:,:,s) = obj.CalcStaticBayesPost(mean(tempISS,3), obj.fisSeqSpikeMtx(:,:,s));
+            end
+            [tempDecodeOdrTm, tempDecodeFISpost] = obj.DecodeBayesPost(obj.fisSeqPosts,obj.fisSeqSpikeOdorLog);
+            obj.fisL1OdecodeOdrTime = nan(size(tempDecodeOdrTm,1),4);
+            for o = 1:4
+                obj.fisL1OdecodeOdrTime(:,o) = sum(tempDecodeOdrTm==o,2)./sum(~isnan(tempDecodeOdrTm),2);
+            end
+            [tempDecodeTime, ~] = obj.DecodeBayesPost(obj.fisSeqPosts, obj.fisSeqSpikeTimeLog);
+            for s = 1:size(tempDecodeTime,2)
+                tempDecodeTime(:,s) = tempDecodeTime(:,s)-obj.fisSeqSpikeTimeLog;
+            end
+            obj.fisL1OdecodeTimeTime = tempDecodeTime;
+            
+            obj.fisL1OdecodeOdr = nan(4);
+            obj.fisL1OdecodeTime = nan(4);
+            obj.fisL1OdecodePosts = nan(4);
+            obj.fisL1OdecodeOdr_TrlPrd = nan(4,4,4);
+            obj.fisL1OdecodeTime_TrlPrd = nan(4,4,4);
+            obj.fisL1OdecodePosts_TrlPrd = nan(4,4,4);
+            for o1 = 1:4
+                for o2 = 1:4
+                    odrMtx = tempDecodeOdrTm(obj.fisSeqSpikeOdorLog==o1,:);
+                    timeMtx = tempDecodeTime(obj.fisSeqSpikeOdorLog==o1,:);
+                    postMtx = tempDecodeFISpost(obj.fisSeqSpikeOdorLog==o1,:);
+                    obj.fisL1OdecodeOdr(o2,o1) = mean(mean(odrMtx==o2));
+                    obj.fisL1OdecodeTime(o2,o1) = mean(timeMtx(odrMtx==o2));
+                    obj.fisL1OdecodePosts(o2,o1) = mean(postMtx(odrMtx==o2));
+                    for prd = 1:4
+                        obj.fisL1OdecodeOdr_TrlPrd(o2,o1,prd) = mean(mean(odrMtx(obj.trialPeriodTimeLog==prd,:)==o2));
+                        obj.fisL1OdecodeTime_TrlPrd(o2,o1,prd) = mean(timeMtx(odrMtx(obj.trialPeriodTimeLog==prd,:)==o2));
+                        obj.fisL1OdecodePosts_TrlPrd(o2,o1,prd) = mean(postMtx(odrMtx(obj.trialPeriodTimeLog==prd,:)==o2));
+                    end
+                end
+            end
+        end
+        %% Compile Trial Observations (Combine PokeIn and PokeOut across trials)
+        function CompileTrialObsvs(obj)
+            obj.trialPeriodTimeLog = [[obj.pokeInTrialTime<0; false(size(obj.pokeOutTrialTime))],...
+                [obj.pokeInTrialTime>0; false(size(obj.pokeOutTrialTime))],...
+                [false(size(obj.pokeInTrialTime)); obj.pokeOutTrialTime<0],...
+                [false(size(obj.pokeInTrialTime)); obj.pokeOutTrialTime>0]];
+            obj.trialPeriodTimeLog = obj.trialPeriodTimeLog*(1:4)';
+            obj.trialPeriodTimeLog(obj.trialPeriodTimeLog==0) = nan;
+            obj.trialTimeLog = [obj.pokeInTrialTime; obj.pokeOutTrialTime+1+obj.dsRate/obj.sampleRate];
+            obj.osTrlNums = [obj.trialInfo([obj.trialInfo.TranspositionDistance]~=0).TrialNum];
+            obj.osTrlNums([obj.trialInfo(obj.osTrlNums).Performance]==0) = [];
+            obj.skpTrlNums = obj.osTrlNums([obj.trialInfo(obj.osTrlNums).TranspositionDistance]<0);
+            obj.repTrlNums = obj.osTrlNums([obj.trialInfo(obj.osTrlNums).TranspositionDistance]>0);
+            obj.taoTrlNums = obj.osTrlNums(([obj.trialInfo(obj.osTrlNums).Position]+1 == [obj.trialInfo(obj.osTrlNums+1).Position])...
+                & [obj.trialInfo(obj.osTrlNums+1).Performance]==1)+1;
+            obj.taRepTrlNums = obj.osTrlNums(([obj.trialInfo(obj.osTrlNums).Position]+1 == [obj.trialInfo(obj.osTrlNums+1).Position])...
+                & [obj.trialInfo(obj.osTrlNums+1).Performance]==1 ...
+                & [obj.trialInfo(obj.osTrlNums).TranspositionDistance]>0)+1;
+            obj.taSkpTrlNums = obj.osTrlNums(([obj.trialInfo(obj.osTrlNums).Position]+1 == [obj.trialInfo(obj.osTrlNums+1).Position])...
+                & [obj.trialInfo(obj.osTrlNums+1).Performance]==1 ...
+                & [obj.trialInfo(obj.osTrlNums).TranspositionDistance]<0)+1;
+            obj.trialSpikeMtx = cell(size(obj.trialInfo));
+            for t = 1:length(obj.trialInfo)
+                obj.trialSpikeMtx{t} = [obj.pokeInTrialMtx(:,:,t); obj.pokeOutTrialMtx(:,:,t)];
+            end
+        end
+        %% Compile Fully InSeq Likelihoods (trial PSTHs)
+        function CompileFISlikes(obj)
+            if isempty(obj.pokeInTrialMtx)
+                error('Compile Poke In data');
+            elseif isempty(obj.pokeOutTrialMtx)
+                error('Complie Poke Out data');
+            end
+            if isempty(obj.trialPeriodTimeLog)
+                obj.CompileTrialObsvs;
+            end
+            fisStarts = find(conv([obj.trialInfo.Odor], 1:4, 'valid')==20 &...
+                conv([obj.trialInfo.Position], 1:4, 'valid')==20 &...
+                conv([obj.trialInfo.Performance], ones(1,4), 'valid')==4);
+            obj.fisSeqTrlNums = nan(4,length(fisStarts));
+            for iS = 1:length(fisStarts)
+                obj.fisSeqTrlNums(1,iS) = fisStarts(iS);
+                obj.fisSeqTrlNums(2,iS) = fisStarts(iS) + 1;
+                obj.fisSeqTrlNums(3,iS) = fisStarts(iS) + 2;
+                obj.fisSeqTrlNums(4,iS) = fisStarts(iS) + 3;
+            end
+            obj.fisSeqSpikeMtx = nan((size(obj.pokeInTrialMtx,1) + size(obj.pokeOutTrialMtx,1))*4, size(obj.pokeInTrialMtx,2), length(fisStarts));
+            for seq = 1:length(fisStarts)
+                obj.fisSeqSpikeMtx(:,:,seq) = [obj.pokeInTrialMtx(:,:,obj.fisSeqTrlNums(1,seq)); obj.pokeOutTrialMtx(:,:,obj.fisSeqTrlNums(1,seq));...
+                    obj.pokeInTrialMtx(:,:,obj.fisSeqTrlNums(2,seq)); obj.pokeOutTrialMtx(:,:,obj.fisSeqTrlNums(2,seq));...
+                    obj.pokeInTrialMtx(:,:,obj.fisSeqTrlNums(3,seq)); obj.pokeOutTrialMtx(:,:,obj.fisSeqTrlNums(3,seq));...
+                    obj.pokeInTrialMtx(:,:,obj.fisSeqTrlNums(4,seq)); obj.pokeOutTrialMtx(:,:,obj.fisSeqTrlNums(4,seq))];
+            end
+            obj.fisSeqSpikeOdorLog = [ones(1,size(obj.pokeInTrialMtx,1)+size(obj.pokeOutTrialMtx,1))*1,...
+                ones(1,size(obj.pokeInTrialMtx,1)+size(obj.pokeOutTrialMtx,1))*2,...
+                ones(1,size(obj.pokeInTrialMtx,1)+size(obj.pokeOutTrialMtx,1))*3,...
+                ones(1,size(obj.pokeInTrialMtx,1)+size(obj.pokeOutTrialMtx,1))*4]';
+            obj.fisSeqSpikeTimeLog = repmat([obj.pokeInTrialTime; obj.pokeOutTrialTime+1+obj.dsRate/obj.sampleRate], [4,1]);
+            obj.fisSeqSpikeTrialPeriodLog = repmat(obj.trialPeriodTimeLog, [4,1]);
+        end
+        %% Extract Matrix
+        function CompileMLBmtx(obj, alignment)
+            switch alignment
+                case 'PokeIn'
+                    [obj.pokeInTrialMtx, obj.pokeInTrialTime] = obj.PP_TrialMatrix(obj.pokeInWindow, 'PokeIn');
+                case 'PokeOut'
+                    [obj.pokeOutTrialMtx, obj.pokeOutTrialTime] = obj.PP_TrialMatrix(obj.pokeOutWindow, 'PokeOut');
+            end
+        end
+    end
+end
