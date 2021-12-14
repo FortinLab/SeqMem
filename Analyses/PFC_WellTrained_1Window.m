9213226048d20eb7f9609ca4e235ab740dbbe5cb
Gabriel Elias <g.a.elias@gmail.com>
2021-12-13 21:23:05 -0800

    Main loop complete

--- Analyses/PFC_WellTrained_1Window.m
+++ Analyses/PFC_WellTrained_1Window.m
@@ -10,11 +10,13 @@ fileDirs = [{'D:\WorkBigDataFiles\PFC\GE11_Session132'},...
     {'D:\WorkBigDataFiles\PFC\GE17_Session095'},...
     {'D:\WorkBigDataFiles\PFC\Session096'}];
 
-binSize = 200;
-dsRate = 50;
+binSize = 100;
+dsRate = 20;
 trlWindow = {[-800 1200]};
 alignment = {'PokeIn'};
 lfpWindow = [16 32];
+numPerms = 10;
+ssProportion = 0.7;
 ssType = 0; % 0 = use all ISC for decoding; 1 = use subsampled ISC types
 bayesType = 1; %1 = Poisson: use with raw spike counts; 2 = Bernoulli: use with binarized spike counts; 3 = Gaussian: Use with z-scored spike counts
 
@@ -24,10 +26,10 @@ fiscLOO_Posts = cell(size(fileDirs));
 fiscLOO_Decodes = cell(3,1,length(fileDirs));
 % fiscISC analysis
 fiscISC_Posts = cell(size(fileDirs));
-fiscISC_Decodes = cell(size(fileDirs));
+fiscISC_Decodes = cell(3,1,length(fileDirs));
 % iscBS analysis
 iscBS_Posts = cell(size(fileDirs));
-iscBS_Decodes = cell(size(fileDirs));
+iscBS_Decodes = cell(3,1,length(fileDirs));
 %%
 for ani = 1:length(fileDirs)
     %% Create initial object & set object parameters
@@ -36,6 +38,8 @@ for ani = 1:length(fileDirs)
     mlb.dsRate = dsRate;
     mlb.windows = trlWindow;
     mlb.alignments = alignment;
+    mlb.numPerms = numPerms;
+    mlb.ssProportion = ssProportion;
     mlb.ssType = ssType;
     mlb.bayesType = bayesType;
     
@@ -49,7 +53,7 @@ for ani = 1:length(fileDirs)
     timePoints = unique(mlb.decodeIDvects{1}(:,1));
     timeAccuracy = nan(size(timeDecode,1), length(timePoints));
     for t = 1:length(timePoints)
-        timeAccuracy(:,t) = mean(timeDecode==timePoints(t),2);
+        timeAccuracy(:,t) = mean(timeDecode==timePoints(t),2, 'omitnan');
     end
     fiscLOO_Decodes{1,1,ani} = timeAccuracy;
     % Decode Position
@@ -57,7 +61,7 @@ for ani = 1:length(fileDirs)
     positions = unique(mlb.decodeIDvects{1}(:,3));
     posAccuracy = nan(size(posDecode,1), length(positions));
     for p = 1:length(positions)
-        posAccuracy(:,p) = mean(posDecode==positions(p),2);
+        posAccuracy(:,p) = mean(posDecode==positions(p),2, 'omitnan');
     end
     fiscLOO_Decodes{2,1,ani} = posAccuracy;
     % Decode Odor
@@ -65,19 +69,89 @@ for ani = 1:length(fileDirs)
     odors = unique(mlb.decodeIDvects{1}(:,4));
     odrAccuracy = nan(size(odrDecode,1),length(odors));
     for o = 1:length(odors)
-        odrAccuracy(:,o) = mean(odrDecode==positions(o),2);
+        odrAccuracy(:,o) = mean(odrDecode==odors(o),2, 'omitnan');
     end
     fiscLOO_Decodes{3,1,ani} = odrAccuracy;    
     
     %% Decode ISC via FISC 
     mlb.Process_Observes;
     fiscISC_Posts{ani} = mlb.post;
+    mlb.trialInfo(mlb.postTrlIDs{1})
+    trlOdrVect = [mlb.trialInfo(reshape(mlb.postTrlIDs{1}, [1,numel(mlb.postTrlIDs{1})])).Odor];
+    trlOdrs = unique(trlOdrVect);
+    trlPosVect = [mlb.trialInfo(reshape(mlb.postTrlIDs{1}, [1,numel(mlb.postTrlIDs{1})])).Position];
+    trlPoss = unique(trlPosVect);
+    % Decode Time
+    timeDecode = mlb.DecodeBayesPost(mlb.post{1}, mlb.decodeIDvects{1}(:,1));
+    timePoints = unique(mlb.decodeIDvects{1}(:,1));
+    timeAccuracy = nan(size(timeDecode,1), length(timePoints), length(trlPoss));
+    for pos = 1:length(trlPoss)
+        for t = 1:length(timePoints)
+            timeAccuracy(:,t,pos) = mean(timeDecode(:,trlPosVect==trlPoss(pos))==timePoints(t),2, 'omitnan');
+        end
+    end
+    fiscISC_Decodes{1,1,ani} = timeAccuracy;
+    % Decode Position
+    posDecode = mlb.DecodeBayesPost(mlb.post{1}, mlb.decodeIDvects{1}(:,3));
+    positions = unique(mlb.decodeIDvects{1}(:,3));
+    posAccuracy = nan(size(posDecode,1), length(positions), length(positions));
+    for pos = 1:length(positions)
+        for p = 1:length(positions)
+            posAccuracy(:,p,pos) = mean(posDecode(:,trlPosVect==positions(pos))==positions(p),2, 'omitnan');
+        end
+    end
+    fiscISC_Decodes{2,1,ani} = posAccuracy; 
+    % Decode Odor
+    odrDecode = mlb.DecodeBayesPost(mlb.post{1}, mlb.decodeIDvects{1}(:,4));
+    odors = unique(mlb.decodeIDvects{1}(:,4));
+    odrAccuracy = nan(size(odrDecode,1),length(odors), length(odors));
+    for odr = 1:length(odors)
+        for o = 1:length(odors)
+            odrAccuracy(:,o,odr) = mean(odrDecode(:,trlOdrVect==odors(odr))==odors(o),2, 'omitnan');
+        end
+    end
+    fiscISC_Decodes{3,1,ani} = odrAccuracy;    
     
     %% Decode ISC via Sub-Sampling
     mlb.SetLikes_SubSample;
     mlb.Process_Observes;
-    iscBS_Posts{ani} = mlb.post;
-
+    iscBS_Posts{ani} = cell2mat(reshape(mlb.post, [1,1,numel(mlb.post)]));
+    postTrlIDs = cell2mat(reshape(mlb.postTrlIDs, [1,1,numel(mlb.postTrlIDs)]));
+    
+    trlOdrVect = [mlb.trialInfo(postTrlIDs).Odor];
+    trlOdrs = unique(trlOdrVect);
+    trlPosVect = [mlb.trialInfo(postTrlIDs).Position];
+    trlPoss = unique(trlPosVect);
+    % Decode Time
+    timeDecode = mlb.DecodeBayesPost(iscBS_Posts{ani}, mlb.decodeIDvects{1}(:,1));
+    timePoints = unique(mlb.decodeIDvects{1}(:,1));
+    timeAccuracy = nan(size(timeDecode,1), length(timePoints), length(trlPoss));
+    for pos = 1:length(trlPoss)
+        for t = 1:length(timePoints)
+            timeAccuracy(:,t,pos) = mean(timeDecode(:,trlPosVect==trlPoss(pos))==timePoints(t),2, 'omitnan');
+        end
+    end
+    iscBS_Decodes{1,1,ani} = timeAccuracy;
+    % Decode Position
+    posDecode = mlb.DecodeBayesPost(iscBS_Posts{ani}, mlb.decodeIDvects{1}(:,3));
+    positions = unique(mlb.decodeIDvects{1}(:,3));
+    posAccuracy = nan(size(posDecode,1), length(positions), length(positions));
+    for pos = 1:length(positions)
+        for p = 1:length(positions)
+            posAccuracy(:,p,pos) = mean(posDecode(:,trlPosVect==positions(pos))==positions(p),2, 'omitnan');
+        end
+    end
+    iscBS_Decodes{2,1,ani} = posAccuracy; 
+    % Decode Odor
+    odrDecode = mlb.DecodeBayesPost(iscBS_Posts{ani}, mlb.decodeIDvects{1}(:,4));
+    odors = unique(mlb.decodeIDvects{1}(:,4));
+    odrAccuracy = nan(size(odrDecode,1),length(odors), length(odors));
+    for odr = 1:length(odors)
+        for o = 1:length(odors)
+            odrAccuracy(:,o,odr) = mean(odrDecode(:,trlOdrVect==odors(odr))==odors(o),2, 'omitnan');
+        end
+    end
+    iscBS_Decodes{3,1,ani} = odrAccuracy;    
 end
 
 figure;
